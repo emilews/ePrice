642516e9481145cecd5525b261fc7ccbd8983ae4
Emilio Wong <emilews.ew@gmail.com>
2018-05-31 11:36:56 -0700

    Moved sxript entirely to data.js from index.html

--- js/data.js
+++ js/data.js
@@ -2,3 +2,6 @@ var request = require('request')
 
 request({uri: 'https://api.coinmarketcap.com/v2/ticker/2137/'});
 
+var data = JSON.parse(body)
+document.getElementById('name').innerHTML = data.data.name
+document.getElementById('rank').innerHTML = data.data.rank
