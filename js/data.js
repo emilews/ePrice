6319cbbb1accf3c602e39b06b88a4650a48ea59a
Emilio Wong <emilews.ew@gmail.com>
2018-05-31 11:44:44 -0700

    Rolled back script to html and data.js no longer exists

--- js/data.js
+++
@@ -1,7 +0,0 @@
-var request = require('request')
-
-request({uri: 'https://api.coinmarketcap.com/v2/ticker/2137/'});
-
-var data = JSON.parse(body)
-document.getElementById('name').innerHTML = data.data.name
-document.getElementById('rank').innerHTML = data.data.rank
